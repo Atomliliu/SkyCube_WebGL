853b476fc93b09b5fc8b8184d8cd678d9e81f6bb
Atom li liu <atom.li.liu@gmail.com>
2016-11-30 15:33:53 -0500

    Update SC_SL2.js

--- SC_SL2.js
+++ SC_SL2.js
@@ -226,3 +226,140 @@ THREE.SCSL_LL2CUBE3 = {
 
 };
 
+
+
+
+THREE.SCSL_LL2CUBE_UI = {
+
+
+	uniforms: {
+
+		tSampler: 	 { type: "t", value: null },
+		face:	 { type: "i", value: 0 },
+	},
+
+
+	vertexShader: [
+
+		"varying vec3 vWorldPosition;",
+		"varying vec2 vUv;",
+
+		"void main() {",
+
+			"vec4 worldPosition = modelMatrix * vec4( position, 1.0 );",
+			"vWorldPosition = worldPosition.xyz;",
+			"vUv = uv;",
+			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+		"}",
+
+
+	].join( "\n" ),
+
+	fragmentShader: [
+
+		"uniform sampler2D tSampler;",
+		"varying vec3 vWorldPosition;",
+		"varying vec2 vUv;",
+
+		"uniform int face;",
+
+		"#define A_PI		3.14159265358", //3.1415926535897932384626433832795
+		"#define A_1D_PI		0.31830988618", //0.31830988618379067153776752674503
+		"#define A_1D6		0.166666666667", //0.16666666666666666666666666666667
+
+		//"#include <packing>",
+
+		//Invead X axis from Unity shader
+
+	    "vec3 getVec2(vec2 UV){",
+			"if (UV.x>=0.0 && UV.x<A_1D6){",
+				"vec2 UV2 = UV;",
+				"UV2.x = UV2.x * 6.0;",
+				"return getVec(UV2,0);",
+			"}",
+			"else if (UV.x>=A_1D6 && UV.x<(A_1D6*2.0)){",
+				"vec2 UV2 = UV;",
+				"UV2.x = (UV2.x-A_1D6) * 6.0;",
+				"return getVec(UV2,1);",
+			"} ",
+			"else if (UV.x>=(A_1D6*2.0) && UV.x<(A_1D6*3.0)){",
+				"vec2 UV2 = UV;",
+				"UV2.x = (UV2.x-(A_1D6*2.0)) * 6.0;",
+				"return getVec(UV2,2);",
+			"} ",
+			"else if (UV.x>=(A_1D6*3.0) && UV.x<(A_1D6*4.0)){",
+				"vec2 UV2 = UV;",
+				"UV2.x = (UV2.x-(A_1D6*3.0)) * 6.0;",
+				"return getVec(UV2,3);",
+			"} ",
+			"else if (UV.x>=(A_1D6*4.0) && UV.x<(A_1D6*5.0)){",
+				"vec2 UV2 = UV;",
+				"UV2.x = (UV2.x-(A_1D6*4.0)) * 6.0;",
+				"return getVec(UV2,4);",
+			"} ",
+			"else if (UV.x>=(A_1D6*5.0) && UV.x<=1.0){",
+				"vec2 UV2 = UV;",
+				"UV2.x = (UV2.x-(A_1D6*5.0)) * 6.0;",
+				"return getVec(UV2,5);",
+			"} ",
+
+			"return vec3(0);",
+			
+		"}",
+
+
+	    "vec2 getLLMapping_VEC2UV(vec3 vec) //Use for create LP map",
+		"{",
+			"vec2 UV;",
+
+			"UV.y = acos(-vec.y) * A_1D_PI; // y = 1 to -1, v = 0 to PI",
+
+			"float P = abs(vec.x/vec.z);",
+			//float O = 0.0f;
+
+			"if (vec.x >= 0.0) {",
+				"if(vec.z == 0.0) {",
+					"UV.x = 0.5;",
+				"}",
+				"else if(vec.z < 0.0) {",
+					"UV.x = (A_PI - atan(P)) * A_1D_PI;",
+				"}",
+				"else {",
+					"UV.x = atan(P) * A_1D_PI;",
+				"}",
+
+			"}",
+			"else { // X < 0  //phase",
+				"if(vec.z == 0.0) {",
+					"UV.x = -0.5;",
+				"}",
+				"else if(vec.z < 0.0) {",
+					"UV.x = -(A_PI - atan(P)) * A_1D_PI;",
+				"}",
+				"else {",
+					"UV.x = -atan(P) * A_1D_PI;",
+				"}",
+			"}",
+
+			"UV.x = (UV.x + 1.0) * 0.5;",
+
+			"return UV;",
+		"}",
+
+
+		"void main() {",
+
+			//"vec4 frag(v2f i) : COLOR ",
+			"{",
+				//"vec2 UV = vUv;",
+				"vec4 result = texture2D( tSampler,  getLLMapping_VEC2UV( getVec2(vUv) ) );",
+
+				"gl_FragColor = result;",
+
+			"}",
+
+		"}",
+
+	].join( "\n" )
+
+};
