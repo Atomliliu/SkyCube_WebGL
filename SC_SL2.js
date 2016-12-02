c70704ef4d17c1545aca07e065d48ed596b501c8
Atom li liu <atom.li.liu@gmail.com>
2016-12-02 16:41:09 -0500

    Update SC_SL2.js

--- SC_SL2.js
+++ SC_SL2.js
@@ -91,7 +91,7 @@ THREE.SCSL_LL2CUBE2 = {
 			"}",
 			"else if (UV.x>=A_1D6 && UV.x<A_2D6){",
 				"vec2 UV2 = UV;",
-				"UV2.x = (UV2.x-A_1D6) * 6.0;",
+				"UV2.x = clamp((UV2.x-A_1D6) * 6.0,0.0,1.0);",
 				"return getVec(UV2,1);",
 			"} ",
 			"else if (UV.x>=A_2D6 && UV.x<A_3D6){",
@@ -270,6 +270,10 @@ THREE.SCSL_LL2CUBE_UI = {
 		"#define A_PI		3.14159265358", //3.1415926535897932384626433832795
 		"#define A_1D_PI		0.31830988618", //0.31830988618379067153776752674503
 		"#define A_1D6		0.166666666667", //0.16666666666666666666666666666667
+		"#define A_2D6		0.333333333333", //0.33333333333333333333333333333333
+		"#define A_3D6		0.5",
+		"#define A_4D6		0.666666666667", //0.66666666666666666666666666666667
+		"#define A_5D6		0.833333333333", //0.83333333333333333333333333333333
 
 		//"#include <packing>",
 
@@ -316,36 +320,60 @@ THREE.SCSL_LL2CUBE_UI = {
 	    "vec3 getVec2(vec2 UV){",
 			"if (UV.x>=0.0 && UV.x<A_1D6){",
 				"vec2 UV2 = UV;",
-				"UV2.x = UV2.x * 6.0;",
+				"UV2.x = clamp(UV2.x * 6.0,0.0,1.0);",
 				"return getVec(UV2,0);",
 			"}",
-			"else if (UV.x>=A_1D6 && UV.x<(A_1D6*2.0)){",
+			"else if (UV.x>=A_1D6 && UV.x<A_2D6){",
 				"vec2 UV2 = UV;",
-				"UV2.x = (UV2.x-A_1D6) * 6.0;",
+				"UV2.x = clamp(UV2.x * 6.0 - 1.0,0.0,1.0);",
 				"return getVec(UV2,1);",
 			"} ",
-			"else if (UV.x>=(A_1D6*2.0) && UV.x<(A_1D6*3.0)){",
+			"else if (UV.x>=A_2D6 && UV.x<A_3D6){",
 				"vec2 UV2 = UV;",
-				"UV2.x = (UV2.x-(A_1D6*2.0)) * 6.0;",
+				"UV2.x = clamp(UV2.x*6.0 - 2.0,0.0,1.0);",
 				"return getVec(UV2,2);",
 			"} ",
-			"else if (UV.x>=(A_1D6*3.0) && UV.x<(A_1D6*4.0)){",
+			"else if (UV.x>=A_3D6 && UV.x<A_4D6){",
 				"vec2 UV2 = UV;",
-				"UV2.x = (UV2.x-(A_1D6*3.0)) * 6.0;",
+				"UV2.x = clamp(UV2.x * 6.0 - 3.0,0.0,1.0);",
 				"return getVec(UV2,3);",
 			"} ",
-			"else if (UV.x>=(A_1D6*4.0) && UV.x<(A_1D6*5.0)){",
+			"else if (UV.x>=A_4D6 && UV.x<A_5D6){",
 				"vec2 UV2 = UV;",
-				"UV2.x = (UV2.x-(A_1D6*4.0)) * 6.0;",
+				"UV2.x = clamp(UV2.x*6.0 - 4.0,0.0,1.0);",
 				"return getVec(UV2,4);",
 			"} ",
-			"else if (UV.x>=(A_1D6*5.0) && UV.x<=1.0){",
+			"else if (UV.x>=A_5D6 && UV.x<=1.0){",
 				"vec2 UV2 = UV;",
-				"UV2.x = (UV2.x-(A_1D6*5.0)) * 6.0;",
+				"UV2.x = clamp(UV2.x * 6.0 - 5.0,0.0,1.0);",
 				"return getVec(UV2,5);",
 			"} ",
 
-			"return vec3(0);",
+			"return vec3(0.0,0.0,1.0);",
+			
+		"}",
+
+		"vec3 getVec3(vec2 UV){",
+			"if (UV.x>=0.0 && UV.x<A_1D6){",
+				"return vec3(1.0,0.0,1.0);",
+			"}",
+			"else if (UV.x>=A_1D6 && UV.x<A_2D6){",
+				"return vec3(0.0,0.0,1.0);",
+			"} ",
+			"else if (UV.x>=A_2D6 && UV.x<A_3D6){",
+				"return vec3(0.0,1.0,1.0);",
+			"} ",
+			"else if (UV.x>=A_3D6 && UV.x<A_4D6){",
+				"return vec3(0.0,1.0,0.0);",
+			"} ",
+			"else if (UV.x>=A_4D6 && UV.x<A_5D6){",
+				"return vec3(1.0,1.0,0.0);",
+			"} ",
+			"else if (UV.x>=A_5D6 && UV.x<=1.0){",
+				"return vec3(1.0,0.0,0.0);",
+			"} ",
+
+			"return vec3(0.0,0.0,0.0);",
 			
 		"}",
 
@@ -395,7 +423,7 @@ THREE.SCSL_LL2CUBE_UI = {
 			"{",
 				//"vec2 UV = vUv;",
 				"vec4 result = texture2D( tSampler,  getLLMapping_VEC2UV( getVec2(vUv) ) );",
-
+				//"result.rgb = getVec2(vUv);",
 				"gl_FragColor = result;",
 
 			"}",
