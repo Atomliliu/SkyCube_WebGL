22aa3d21966f2724583fa5c8da5e907b31196e11
Atom li liu <atom.li.liu@gmail.com>
2016-12-12 17:54:33 -0500

    Create SC_ShaderChunk.js

---
+++ SC_ShaderChunk.js
@@ -0,0 +1,13 @@
+import sc_common from './SC_ShaderChunk/sc_common.glsl';
+
+
+import SCSL_LL2CUBE_FRAG from './SC_ShaderLib/SCSL_LL2CUBE_FRAG.glsl';
+import SCSL_LL2CUBE_VERT from './SC_ShaderLib/SCSL_LL2CUBE_VERT.glsl';
+
+
+export var SC_ShaderChunk = {
+	sc_common: sc_common,
+	
+	SCSL_LL2CUBE_FRAG: SCSL_LL2CUBE_FRAG,
+	SCSL_LL2CUBE_VERT: SCSL_LL2CUBE_VERT
+};
