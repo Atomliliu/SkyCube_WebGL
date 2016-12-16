ad6ab31654423bd70667adcb64077b5b772367ce
Atom li liu <atom.li.liu@gmail.com>
2016-12-16 16:21:23 -0500

    Update sc_common.glsl

--- SC_ShaderChunk/sc_common.glsl
+++ SC_ShaderChunk/sc_common.glsl
@@ -48,6 +48,8 @@ vec3 getVec(vec2 UV, int face){
     return normalize(VEC);
 }
 
+
+
 vec3 getVecHorizonCubeMap(vec2 UV){
 	vec2 UV2 = UV;
 	//UV2.x = clamp(UV2.x,0.0,1.0);
@@ -61,6 +63,33 @@ vec3 getVecHorizonCubeMap(vec2 UV){
 	
 }
 
+vec3 getVecVerticalCubeMap(vec2 UV){
+
+}
+
+vec3 getVecHCrossCubeMap(vec2 UV){
+
+}
+
+vec3 getVecVCrossCubeMap(vec2 UV){
+
+}
+
+vec3 getVecLLCubeMap(vec2 UV){
+
+}
+
+vec3 getVecLPCubeMap(vec2 UV){
+
+}
+
+vec3 getVecDPCubeMap(vec2 UV){
+
+}
+
+vec3 getVecSPCubeMap(vec2 UV){
+
+}
 
 
 vec2 getLLMapping_VEC2UV(vec3 vec) //Use for create LP map",
@@ -101,3 +130,106 @@ vec2 getLLMapping_VEC2UV(vec3 vec) //Use for create LP map",
 	return vec2(UV);
 }
 
+
+
+vec2 getLPMapping_VEC2UV(vec3 vec) //Use for create LP map
+{
+	vec2 UV;
+	float  th, la, lr, L, P;
+
+	//UV = UV * 2 - 1; // Range to -1 to 1
+
+	if(vec.z == 1.0){
+		"UV.x = UV.y = 0.0;
+	}
+
+	else {
+		th = sqrt(vec.x * vec.x + vec.y * vec.y);
+		if(vec.z < 0.0f) {
+			la = asin(th);
+			lr = (A_PI - la) * A_1D_PI;
+			UV.y = lr * (vec.y / th);
+			UV.x = lr * (vec.x / th);
+		}
+
+		else{
+			la = asin(th);
+			lr = la * A_1D_PI;
+			UV.y = lr * (vec.y / th);
+			UV.x = lr* (vec.x / th);
+		}
+		
+		//lr = pow(L * L + P * P, 0.5f); 
+	}
+
+	//From -1 to 1 move to 0 to 1 range
+	UV = (UV + 1.0) * 0.5;
+
+	if(mode > 0.9){ //sky to cube
+		UV.x = (1.0 - UV.x);
+	}
+
+	return UV;
+}
+
+
+
+vec2 getDPUVByVec(vec3 vec)
+{
+	vec2 uv;
+	if(vec.z < 0){ // Front
+		"uv = vec.xy/(1 - vec.z);
+		"uv = (uv + 1) * 0.5; // Range from -1 to 1 to 0 to 1
+		"uv.x *= 0.5; // Move to left in texture
+	}
+	else{ // Back
+		vec.y = -vec.y;
+		uv = - vec.xy/(1 + vec.z);
+		uv = (uv + 1) * 0.5; // Range from -1 to 1 to 0 to 1
+		//uv.y = -uv.y;
+		uv.x = (uv.x * 0.5) + 0.5; // Move to right in texture
+	}
+
+	return uv;
+}
+
+
+vec3 getVecByDPUV(vec2 uv){
+	float A;
+	vec3 vec;
+	if(uv.x < 0.5){
+		uv.x = uv.x * 2.0;
+		uv = uv * 2 - 1; // Range from 0 to 1 to 1 to -1 (since in unity UV is invert by directX UV)
+		uv *= 1.2;
+		A = uv.x * uv.x + uv.y * uv.y + 1;
+		vec = vec3(2*uv.x,2*uv.y,(A-2)); // -1+s^2+t^2 = A-2
+		return (vec/A);
+	}
+	else {
+		uv.x = uv.x * 2.0 - 1.0;
+
+		uv = uv * 2 - 1; // Range from 0 to 1 to 1 to -1 (since in unity UV is invert by directX UV)
+		uv *= 1.2;
+		A = uv.x * uv.x + uv.y * uv.y + 1;
+		vec = vec3(2*uv.x,-2*uv.y,(A-2)); // -1+s^2+t^2 = A-2
+		return (-vec/A);
+	}
+}
+
+
+
+
+vec3 DecodeRGBM(vec4 rgbm, float maxRange, float lum)
+{
+	return rgbm.rgb * (rgbm.a * maxRange) * lum;
+}
+
+
+
+vec4 EncodeRGBM(vec3 rgb, float maxRange)
+{
+    float maxRGB = max(rgb.x,max(rgb.g,rgb.b));
+    float M = maxRGB / maxRange;
+    M = ceil(M * 255.0) / 255.0;
+    return vec4(rgb / (M * maxRange), M);
+}
