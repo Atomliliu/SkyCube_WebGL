d42a5bba8f939a2a8c96860ceaad2cb442074d14
Atom li liu <atom.li.liu@gmail.com>
2017-01-07 10:42:43 +0800

    Update SCSL_LL2CUBE_VERT.glsl

--- SC_ShaderLib/SCSL_LL2CUBE_VERT.glsl
+++ SC_ShaderLib/SCSL_LL2CUBE_VERT.glsl
@@ -5,12 +5,10 @@ varying vec2 vUv;
 
 void main() {
 
-	void main() {
+	vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+	vWorldPosition = worldPosition.xyz;
+	vUv = uv;
+	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
-		vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
-		vWorldPosition = worldPosition.xyz;
-		vUv = uv;
-		gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-	}
 
 }
