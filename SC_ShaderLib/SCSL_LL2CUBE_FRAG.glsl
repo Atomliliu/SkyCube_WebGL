c3e3fb9ceb8dddaa284f9bd56998f2ab3e08502f
Atom li liu <atom.li.liu@gmail.com>
2017-01-23 15:30:28 -0500

    Update SCSL_LL2CUBE_FRAG.glsl

--- SC_ShaderLib/SCSL_LL2CUBE_FRAG.glsl
+++ SC_ShaderLib/SCSL_LL2CUBE_FRAG.glsl
@@ -9,12 +9,12 @@ uniform int nFace;
 void main() {
 
 	//vec4 frag(v2f i) : COLOR 
-	{
+	//{
 		//vec2 UV = vUv;
 		vec4 result = texture2D( tSampler,  getLLMapping_VEC2UV( getVec(vUv,nFace) ) );
 
 		gl_FragColor = result;
 
-	}
+	//}
 
 }
