6764c3dc3637a5881898f72b7c410cf0258574c6
Atom li liu <atom.li.liu@gmail.com>
2017-01-26 11:54:30 -0500

    Update SCSL_RASTER_VERT.glsl

--- SC_ShaderLib/SCSL_RASTER_VERT.glsl
+++ SC_ShaderLib/SCSL_RASTER_VERT.glsl
@@ -3,11 +3,14 @@
 varying vec3 vWorldPosition;
 varying vec2 vUv;
 
+uniform vec2 vUvFlip;
+
 void main() {
 
 	vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
 	vWorldPosition = worldPosition.xyz;
-	vUv = uv;
+
+	vUv = abs(vUvFlip-uv);
 	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
 
