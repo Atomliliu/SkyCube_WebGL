b34d9d27e4aeca8d90fce5a46b32276d8a0f76b2
Atom li liu <atom.li.liu@gmail.com>
2017-02-06 17:52:36 -0500

    Create SCSL_ENV2LP_HUD_FRAG.glsl

---
+++ SC_ShaderLib/SCSL_ENV2LP_HUD_FRAG.glsl
@@ -0,0 +1,21 @@
+#include <sc_common>
+
+uniform samplerCube tCube;
+varying vec3 vWorldPosition;
+varying vec2 vUv;
+
+//uniform int nFace;
+uniform float fFlip;
+uniform float fOpacity;
+
+void main() {
+	vec3 dir = getVecLPCubeMap(vUv, 0);
+	vec4 result = textureCube( tCube, vec3( fFlip * dir.x, dir.yz ) );
+
+	//vec4 result = texture2D( tSampler,  getLLMapping_VEC2UV( getVec(vUv,nFace) ) );
+
+	gl_FragColor = result;
+
+	gl_FragColor.a *= fOpacity;
+
+}
