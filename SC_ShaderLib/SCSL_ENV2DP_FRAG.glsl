547f2388ed9af010deb15c184d96029b11f42b22
Atom li liu <atom.li.liu@gmail.com>
2017-02-06 17:56:11 -0500

    Create SCSL_ENV2DP_FRAG.glsl

---
+++ SC_ShaderLib/SCSL_ENV2DP_FRAG.glsl
@@ -0,0 +1,20 @@
+#include <sc_common>
+
+uniform samplerCube tCube;
+varying vec3 vWorldPosition;
+varying vec2 vUv;
+
+//uniform int nFace;
+uniform float fFlip;
+//uniform float fOpacity;
+
+void main() {
+	vec3 dir = getVecDPCubeMap(vUv, 0);
+	vec4 result = textureCube( tCube, vec3( fFlip * dir.x, dir.yz ) );
+
+
+	gl_FragColor = result;
+
+	//gl_FragColor.a *= fOpacity;
+
+}
