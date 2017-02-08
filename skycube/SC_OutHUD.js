2a2d934550c2191e8c2c7c968f6dc5b235be8750
Atom li liu <atom.li.liu@gmail.com>
2017-02-08 17:29:52 -0500

    Update SC_OutHUD.js

--- skycube/SC_OutHUD.js
+++ skycube/SC_OutHUD.js
@@ -1,32 +1,14 @@
-import { Object3D } from '../core/Object3D';
-import { WebGLRenderTarget } from '../renderers/WebGLRenderTarget';
-import { ShaderMaterial } from '../materials/ShaderMaterial';
-import { MultiMaterial } from '../materials/MultiMaterial';
-import { UniformsUtils } from '../renderers/shaders/UniformsUtils';
-import { ShaderLib } from '../renderers/shaders/ShaderLib';
-//import { _Math } from '../math/Math';
-
-import { Mesh } from '../objects/Mesh';
-import { BoxGeometry } from '../geometries/BoxGeometry';
-import { PlaneGeometry } from '../geometries/PlaneGeometry';
-
-import { LinearFilter, NearestFilter, RGBFormat } from '../constants';
-import { Vector2 } from '../math/Vector2';
-import { Vector3 } from '../math/Vector3';
-import { OrthographicCamera } from '../cameras/OrthographicCamera';
-import { Scene } from '../scenes/Scene';
 
 
 
 
+THREE.SC_OutHUD = function ( cubeMap, width, height ) {
 
-
-
-function SC_OutHUD( cubeMap, width, height ) {
-
-	Object3D.call( this );
+	//Object3D.call( this );
 
 	this.type = 'SC_OutHUD';
+
+	this.enabled = true;
 
 	//var options = { minFilter: LinearFilter, magFilter: NearestFilter, format: RGBFormat };
 
@@ -70,7 +52,7 @@ function SC_OutHUD( cubeMap, width, height ) {
 		1.0,
 		1.0,
 		1.0,
-		1.0,
+		0.7,
 		1.0,
 		1.0,
 		0.75,
@@ -82,7 +64,7 @@ function SC_OutHUD( cubeMap, width, height ) {
 		0.75,
 		0.5,
 		1.0,
-		1.0,
+		0.7,
 		1.0,
 		0.16666666666666666666666666666667,
 		1.0,
@@ -105,41 +87,49 @@ function SC_OutHUD( cubeMap, width, height ) {
 	var UI_width = Math.abs(width);
 	var UI_height = Math.abs(height);
 
-	var layoutNum = new Vector2(0,0);
+	var layoutNum = new THREE.Vector2(0,0);
 	var iconSize = 1.0;
-	var iconEdgeSize = new Vector2(0,0);
-	var iconMinGap = 2.0;
+	var iconEdgeSize = new THREE.Vector2(0,0);
+	var iconMinGap = 8.0;
 
-	var boxUIGeo = new BoxGeometry( 1, 1, 1 );
+	var boxUIGeo = new THREE.BoxGeometry( 1, 1, 1 );
 	var boxUIMats = [];
-	var boxUIFaceIndex = [0,2,4,-1,-1,-1];
+	var boxUIFaceIndex = [0,1,2,3,4,5];
 	//var boxUIMesh;
 
 	
-	// Create the camera and set the viewport to match the screen dimensions.
-	this.cameraHUD = new OrthographicCamera(-UI_width/2, UI_width/2, UI_height/2, -UI_height/2, 0, 30 );
-
+	
 	// Create also a custom scene for HUD.
-	this.sceneHUD = new Scene();
+	this.sceneHUD = new THREE.Scene();
 
 	//Init
 	//Need update size first
-	var boxUIShader = ShaderLib[ "ENV2CUBEFACE_HUD" ];
+	var boxUIShader = THREE.ShaderLib[ "ENV2CUBEFACE_HUD" ];
 	
 	function setupBoxUI(index){
 		//Init box materials (3) +x +y +z
 		for ( var i = 0; i < boxUIFaceIndex.length; i ++ ) {
-			var boxUIUniforms = UniformsUtils.clone( boxUIShader.uniforms );
+			var boxUIUniforms = THREE.UniformsUtils.clone( boxUIShader.uniforms );
 			boxUIUniforms.tCube.value = cubeMap;
 			boxUIUniforms.nFace.value = boxUIFaceIndex[i];
-			boxUIMats.push( new ShaderMaterial({uniforms: boxUIUniforms,
+			boxUIUniforms.fOpacity.value = 1.0;
+			boxUIMats.push( new THREE.ShaderMaterial({uniforms: boxUIUniforms,
 						vertexShader: boxUIShader.vertexShader,
-						fragmentShader: boxUIShader.fragmentShader}));
+						fragmentShader: boxUIShader.fragmentShader,
+						transparent: true,
+						opacity: 0}));
+			//boxUIMats[i].transparent = true;
+			//boxUIMats[i].opacity = 0.0;
+
 		}
 
+		UI_Materials[index] = new THREE.MultiMaterial( boxUIMats );
+		
+
 		//boxUIMesh = new Mesh( boxUIGeo, new MultiMaterial( boxUIMats ) );
-		UI_IconPlane[index] = new Mesh( boxUIGeo, new MultiMaterial( boxUIMats ) );
+		UI_IconPlane[index] = new THREE.Mesh( boxUIGeo, UI_Materials[index]);
 		UI_IconPlane[index].rotation.x = 0.5;
+		UI_IconPlane[index].rotation.y = 0.707106; // cos 45
 		//Better use push replace [index]
 
 	}
@@ -149,19 +139,19 @@ function SC_OutHUD( cubeMap, width, height ) {
 
 	for ( var i = 0; i < UI_ShaderNames.length; i ++ ) {
 		if(UI_ShaderNames[i] == "ENV2CUBEFACE_HUD"){
-			UI_Shaders[i] = ShaderLib[ UI_ShaderNames[i] ];
+			UI_Shaders[i] = THREE.ShaderLib[ UI_ShaderNames[i] ];
 			setupBoxUI(i);
 			
 			//this.sceneHUD.add( UI_IconPlane[i] );
 
 		}
 		else{
-			UI_Shaders[i] = ShaderLib[ UI_ShaderNames[i] ];
-			var uniformsUI = UniformsUtils.clone( UI_Shaders[i].uniforms );
+			UI_Shaders[i] = THREE.ShaderLib[ UI_ShaderNames[i] ];
+			var uniformsUI = THREE.UniformsUtils.clone( UI_Shaders[i].uniforms );
 			uniformsUI.tCube.value = cubeMap;
 			//uniformsUI.fOpacity.value = 0.5;
 
-			UI_Materials[i] = new ShaderMaterial({uniforms: uniformsUI,
+			UI_Materials[i] = new THREE.ShaderMaterial({uniforms: uniformsUI,
 						vertexShader: UI_Shaders[i].vertexShader,
 						fragmentShader: UI_Shaders[i].fragmentShader});
 
@@ -171,7 +161,7 @@ function SC_OutHUD( cubeMap, width, height ) {
 			//Out_Shaders[i] = ShaderLib[ Out_ShaderNames[i] ];
 
 			//UI_IconPos[i] = new Vector2(0,0);
-			UI_IconPlane[i] = new Mesh( new PlaneGeometry( 1, 1 ), UI_Materials[i] );
+			UI_IconPlane[i] = new THREE.Mesh( new THREE.PlaneGeometry( 1, 1 ), UI_Materials[i] );
 			//UI_IconPlane[i] = new Mesh( new PlaneGeometry( iconSize*UI_IconsSizeX[i], iconSize*UI_IconsSizeY[i] ), UI_Materials[i] );
 			//UI_IconPlane[i].position.x = UI_IconPos[i].x;
 			//UI_IconPlane[i].position.y = UI_IconPos[i].y;
@@ -185,6 +175,10 @@ function SC_OutHUD( cubeMap, width, height ) {
 
 	updateHUDSize();
 	updateIconPlaneSize();
+
+	// Create the camera and set the viewport to match the screen dimensions.
+	this.cameraHUD = new THREE.OrthographicCamera(-UI_width/2, UI_width/2, UI_height/2, -UI_height/2, 0, iconSize+50 );
+
 
 
 	this.highlitHUD = function ( matSelected ) {
@@ -222,14 +216,13 @@ function SC_OutHUD( cubeMap, width, height ) {
 		var index = 0;
 		for ( var iy = 0; iy < layoutNum.y; iy ++ ) {
 			for ( var ix = 0; ix < layoutNum.x; ix ++ ) {
-				UI_IconPos[index] = new Vector2(0,0);
+				UI_IconPos[index] = new THREE.Vector2(0,0);
 
 				UI_IconPos[index].x = Math.round(iconEdgeSize.x * (ix + 0.5) - (UI_width*0.5));
 				UI_IconPos[index].y = Math.round(iconEdgeSize.y * (iy + 0.5) - (UI_height*0.5));
 				index++;
 			}
 			
-
 		}
 	}
 
@@ -239,8 +232,19 @@ function SC_OutHUD( cubeMap, width, height ) {
 				//UI_IconPlane[i].position.x = UI_IconPos[i].x;
 				//UI_IconPlane[i].position.y = UI_IconPos[i].y;
 
-				UI_IconPlane[i].position.copy( UI_IconPos[i] );
-				UI_IconPlane[i].scale.copy( new Vector2(iconSize*UI_IconsSizeX[i], iconSize*UI_IconsSizeY[i]) );
+				UI_IconPlane[i].position.copy( new THREE.Vector3(UI_IconPos[i].x, UI_IconPos[i].y, 0));
+				
+				if(i==4){
+					UI_IconPlane[i].scale.copy( new THREE.Vector3(iconSize*UI_IconsSizeX[i], iconSize*UI_IconsSizeY[i],iconSize*UI_IconsSizeX[i]) );
+					UI_IconPlane[i].position.z = -iconSize;//-iconSize*UI_IconsSizeX[i];
+					//console.log(UI_IconPlane[i].position);
+					//console.log(UI_IconPlane[i].scale);
+					//console.log(UI_IconPlane[i].geometry);
+				}
+				else{
+					UI_IconPlane[i].scale.copy( new THREE.Vector3(iconSize*UI_IconsSizeX[i], iconSize*UI_IconsSizeY[i],1) );
+				}
+				
 			}
 		}
 		
@@ -257,6 +261,14 @@ function SC_OutHUD( cubeMap, width, height ) {
 
 			updateHUDSize();
 			updateIconPlaneSize();
+
+			//-UI_width/2, UI_width/2, UI_height/2, -UI_height/2, 0, 100
+			this.cameraHUD.left = -UI_width/2;
+			this.cameraHUD.right = UI_width/2;
+			this.cameraHUD.top = UI_height/2;
+			this.cameraHUD.bottom  = -UI_height/2;
+			this.cameraHUD.far = iconSize+10;
+			//this.cameraHUD.updateProjectionMatrix ();
 		}
 		
 
@@ -265,12 +277,12 @@ function SC_OutHUD( cubeMap, width, height ) {
 	};
 
 
-}
+};
 
-SC_OutHUD.prototype = Object.create( Object3D.prototype );
-SC_OutHUD.prototype.constructor = SC_OutHUD;
+//SC_OutHUD.prototype = Object.create( Object3D.prototype );
+//SC_OutHUD.prototype.constructor = SC_OutHUD;
 
 
-export { SC_OutHUD };
+//export { SC_OutHUD };
 
 
