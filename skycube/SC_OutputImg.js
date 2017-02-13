d78d832183a16553fa99b8f90210f790cdc8fcac
Atom li liu <atom.li.liu@gmail.com>
2017-02-13 17:29:29 -0500

    Update SC_OutputImg.js

--- skycube/SC_OutputImg.js
+++ skycube/SC_OutputImg.js
@@ -4,16 +4,21 @@ THREE.SC_OutputImg = function ( render, width, height ) {
 	var canvas = document.createElement( 'canvas' );
 	var ctx = canvas.getContext( '2d' );
 
+	canvas.width = width;
+	canvas.height = height;
+
 	this.OutputRT2PNG = function( render, rtOutput ){
-		
 
 		var pixels = new Uint8Array( 4 * rtOutput.width * rtOutput.height );
 		render.readRenderTargetPixels( rtOutput, 0, 0, rtOutput.width, rtOutput.height, pixels );
+		//console.log(rtOutput.width);
+		//console.log(rtOutput.height);
 
 		var imageData = new ImageData( new Uint8ClampedArray( pixels ), rtOutput.width, rtOutput.height );
-
+		//console.log(imageData);
 		ctx.putImageData( imageData, 0, 0 );
 
+		
 		canvas.toBlob( function( blob ) {
 
 			var url = URL.createObjectURL(blob);
