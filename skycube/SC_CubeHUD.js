d852c701cd7fb902af4e2c3010b10165dba2b9cd
Atom li liu <atom.li.liu@gmail.com>
2017-02-21 17:46:11 -0500

    Create SC_CubeHUD.js

---
+++ skycube/SC_CubeHUD.js
@@ -0,0 +1,5 @@
+THREE.SC_CubeHUD = function ( cubeMap, width, height, domElement ) {
+};
+
+THREE.SC_CubeHUD.prototype = Object.create( THREE.EventDispatcher.prototype );
+THREE.SC_CubeHUD.prototype.constructor = THREE.SC_CubeHUD;
