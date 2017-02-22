f100b01ec47fea4e2f9f39a6ae81db833e81e99b
Atom li liu <atom.li.liu@gmail.com>
2017-02-22 17:47:17 -0500

    Update SCFL_InputFiles.js

--- skycube/FL/SCFL_InputFiles.js
+++ skycube/FL/SCFL_InputFiles.js
@@ -3,9 +3,17 @@
 SCFL_InputFiles = function ( idName, typeName ) {
 	var root = this;
 	var div = document.getElementById(idName);
+	//var input;
+	var type = "file";
+	var loadType = [];
 	this.files;
 	
 	this.enabled = true;
+	this.onLoaded = undefined;
+
+	if(typeName != undefined && typeName != ""){
+		type2Array();
+	}
 
 	//Move this func to common
 	function loadjscssfile(fileName, fileType){
@@ -24,10 +32,38 @@ SCFL_InputFiles = function ( idName, typeName ) {
 	        document.getElementsByTagName("head")[0].appendChild(fileRef)
 	}
 
+	//Move it to THREE
+	function type2Array(){
+		loadType = typeName.split(",");
+	}
+
+
+	//Move it to THREE
+	function checkTypeAvailable( fileCheck, fileTypeArray ){
+		var fileExt = fileCheck.name.split(".");
+
+		var fe = fileTypeArray.indexOf("." + fileExt[fileExt.length - 1]);
+		//console.log(fileExt[fileExt.length - 1]);
+		if (fe >= 0) {return true;}
+		else {return false;}
+	}
+
+	//Move it to THREE
+	function checkTypeAvailableInFiles( filesArray, fileTypeArray ){
+		for (var i = 0; i< filesArray.length; i++) {
+	    	var f = filesArray[i];
+	    	
+	    	if (checkTypeAvailable(f,fileTypeArray)){
+	    		return true;
+	    	}
+	    }
+	    return false;
+	}
+
 	//var buttonCSS = new THREE.SC_Common();
 	//buttonCSS.loadjscssfile("../CSS/SC_InputPage.css","css");
 	
-
+	
 
 	function createInputZone(){
 		//div = document.createElement("div");
@@ -52,7 +88,7 @@ SCFL_InputFiles = function ( idName, typeName ) {
 		
 
 		var input = document.createElement("input");
-		input.type="file";
+		input.type=type;
 		input.id="file";
 		input.required = true;
 
@@ -64,15 +100,80 @@ SCFL_InputFiles = function ( idName, typeName ) {
 		div.appendChild(label);
 
 		document.body.appendChild(div);
+
+		label.addEventListener('change', onFileSelect, false );
+	}
+
+	function setupFilesType( idName ){
+		document.getElementById(idName).setAttribute("accept", typeName);
+	}
+
+	function onFileSelect(event) {
+		root.files = event.target.files; // FileList object
+		if(loadType.length >= 1){
+			
+		    if(checkTypeAvailableInFiles(root.files, loadType)){
+		    	if(root.onLoaded) {root.onLoaded();}
+	    		//if(cbFunc) {
+	    		//	cbFunc(root.files);
+	    		//}
+	    		//console.log("good");
+	    	}
+			else{
+				//Alert Window for Filr type
+				//console.log("file type not supported!");
+			}
+		}
+	}
+
+	function removeInputZone(){
+		if(div !== undefined && div.id === "_DropZone"){
+			document.getElementById("file").removeEventListener('change', onFileSelect, false );
+			document.body.removeChild(div);
+			div = undefined;
+		}
+	}
+
+	function shownInputZone(){
+		if(div !== undefined && div.id === "_DropZone"){
+			//document.getElementById("file").addEventListener('change', onFileSelect, false );
+			//document.body.removeChild(div);
+			//div = undefined;
+			div.style.display = true;
+		}
+	}
+
+	function hiddenInputZone(){
+		if(div !== undefined && div.id === "_DropZone"){
+			//document.getElementById("file").removeEventListener('change', onFileSelect, false );
+			//document.body.removeChild(div);
+			//div = undefined;
+			div.style.display = false;
+		}
 	}
 
 	function activate() {
 		loadjscssfile("js/skycube/CSS/SC_InputPage.css","css");
 		createInputZone();
+		setupFilesType(type);
 	}
 
 	function deactivate() {
 		// body...
+		//removeEventListener
+		removeInputZone();
+	}
+
+	function shown() {
+		// body...
+		//removeEventListener
+		shownInputZone();
+	}
+
+	function hidden() {
+		// body...
+		//removeEventListener
+		hiddenInputZone();
 	}
 
 	function dispose() {
@@ -82,5 +183,8 @@ SCFL_InputFiles = function ( idName, typeName ) {
 	this.activate = activate;
 	this.deactivate = deactivate;
 	this.dispose = dispose;
+	this.hidden = hidden;
+	this.shown = shown;
 
 };
+
