77e0eb8937fb31c303ea5f2f9608943daaf927e5
Atom li liu <atom.li.liu@gmail.com>
2017-03-02 02:06:35 -0500

    Create SCFL_HUDClose.js

---
+++ skycube/FL/SCFL_HUDClose.js
@@ -0,0 +1,78 @@
+//
+
+SCFL_HUDClose = function ( idname ) {
+
+	this.enabled = false;
+
+	var root = this;
+	var scc = new THREE.SC_Common();
+	var div;
+
+	this.onClick;
+
+
+	function setupCloseIcon() {
+		var body;
+		if(idname !== undefined && idname != ""){
+			body = document.getElementById(idName)
+		}
+		else{
+			body = document.body;
+		}
+		div = document.createElement("div");
+		div.id = "_close";
+		div.setAttribute("class", "close-global");
+
+		var span = document.createElement("span");
+		span.setAttribute("class", "close");
+		var closeIcon = document.createTextNode("\xD7");
+		span.appendChild(closeIcon);
+
+		div.appendChild(span);
+
+		body.appendChild(div);
+
+		span.onclick = function() {
+			root.deactivate();
+		    if(root.onClick) {root.onClick()};
+		};
+	}
+
+
+	
+
+	function activate() {
+		if(div === undefined){
+			scc.checkloadjscssfile("js/skycube/CSS/SC_InputPanoramaFormatModal.css","css");
+		}
+		this.enabled = true;
+		setupCloseIcon();
+		shown();
+
+	}
+
+	function shown(){
+		div.style.display = "block";
+	}
+
+	function hide(){
+		div.style.display = "none";
+	}
+
+	function deactivate() {
+		div.style.display = "none";
+		scc.removeElements(div);
+		document.body.removeChild(div);
+		this.enabled = false;
+	}
+
+	function dispose() {
+		deactivate();
+	}
+
+	this.activate = activate;
+	this.deactivate = deactivate;
+	this.dispose = dispose;
+
+};
+
