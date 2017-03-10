9e8ec0bda9f70a46203d374ce084a77d0b0a27ee
Atom li liu <atom.li.liu@gmail.com>
2017-03-10 17:25:27 -0500

    Update SC_Controller.js

--- skycube/SC_Controller.js
+++ skycube/SC_Controller.js
@@ -13,22 +13,68 @@ THREE.SC_Controller = function ( dom, css, width, height ) {
 		if(css!=undefined && css!="") elem.setAttribute('class', css);
 	}
 
-	function addElement(parentDom, css, elemName, classType, callBack){
+	function addElement(parentDom, css, elemName, classType, value, id, eventID, callBack){
 		var cb = document.createElement(elemName);
     	cb.setAttribute('type', classType);
     	setCSS(cb,css);
+    	cb.value = value;
+
+    	if(id!=undefined && id!="" && id!=false) cb.id = id;
 
     	if(parentDom==undefined) parentDom = dom;
     	parentDom.appendChild(cb);
-    	cb.onchange = callBack;
+    	if (eventID==0) cb.onchange = callBack;
+    	if (eventID==1) cb.onclick = callBack;
     	root.controllers.push(cb);
     	return cb;
 	}
 
-	this.CheckBox = function (parentDom, css, classType, callBack){
-		if (classType==undefined) classType='checkbox';
-		return addElement(parentDom, css, "input", classType, callBack);
+	this.addCheckBox = function (parentDom, css, value, id, callBack){
+		return addElement(parentDom, css, "input", 'checkbox', value, id, 0, callBack);
 		
+	};
+
+	/*this.addCheckBoxLabeled = function (parentDom, css, value, id, text, callBack){
+		root.addLabel(parentDom, css, id, text);
+		return addElement(parentDom, css, "input", 'checkbox', value, id, 0, callBack);
+		
+	};*/
+
+	this.addButton = function (parentDom, css, value, id, callBack){
+		return addElement(parentDom, css, "input", 'button', value, id, 1, callBack);
+		
+	};
+
+	this.addRadio = function (parentDom, css, value, id, callBack){
+		return addElement(parentDom, css, "input", 'radio', value, id, 0, callBack);
+		
+	};
+
+	this.addNumber = function (parentDom, css, value, id, callBack){
+		return addElement(parentDom, css, "input", 'number', value, id, 0, callBack);
+		
+	};
+
+	this.addRange = function (parentDom, css, value, id, callBack){
+		return addElement(parentDom, css, "input", 'range', value, id, 0, callBack);
+		
+	};
+
+	this.addText = function (parentDom, css, value, id, callBack){
+		return addElement(parentDom, css, "input", 'text', value, id, 0, callBack);
+		
+	};
+
+	this.addLabel = function (parentDom, css, bound, text){
+		var lb = document.createElement('label');
+    	if(bound!=undefined && bound!="" && bound!=false) lb.setAttribute('for', bound);
+    	setCSS(lb,css);
+    	lb.innerHTML = text;
+
+    	if(parentDom==undefined) parentDom = dom;
+    	parentDom.appendChild(lb);
+    	//root.controllers.push(lb);
+    	return lb;
 	};
 
 };
